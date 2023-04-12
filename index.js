12f10b9a80c78f82b6ce9864bf5c59c4421676b8
Mahmoud Abdelkarim <mabdelkarim835@gmail.com>
2023-04-12 17:28:34 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.querySelector("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent= "Mahmoud Abdelkarim is the champion "
+ 
+// For the fourth test . The TextContent and InnerHTML work while the innerText method does not work .
+// Make sure to figure out why innerText does not work  
